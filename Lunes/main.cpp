09a9f68906c855c92a4da76b4b66e65b9cb132f6
Mikhail Shtainer <63235817+FANDIM-wolf@users.noreply.github.com>
2021-01-23 16:50:35 +0300

    Add files via upload

--- Lunes/main.cpp
+++ Lunes/main.cpp
@@ -4,21 +4,34 @@
 #include <sys/stat.h>
 #include "string.h"
 using namespace  std;
+string array_text[] ={}; // array for code of coin
+
 void make_coin(int amount){
-    if (amount >= 100){
-        cout<<"Permison is not availiable "<<endl;
+    int loops = amount; // cycles of making
+    string text_code;
+    while(loops!=0) {
+        loops--;
+
+        if (amount >= 100) {
+            cout << "Permison is not availiable " << endl;
+        }
+        for (int i = 0; i <= 23; i++) {
+            text_code += char(rand() % 26 + 0x61);
+            array_text[i]=text_code;
+        }
     }
+
 }
 int main() {
 
     cout << "Lunes\n\n\n" << std::endl;
     string code_for_coin;
-    // create object to write down data in file
     ofstream file("coin.txt");
+    // create object to write down data in file
     if(file){
         cout<<"There is file "<<endl;
     }
-    for (int i=0;i<=23;i++) {
-        file << char(rand() % 26 + 0x61);
-    }
+    make_coin(1);
+    file<<""<<array_text;
+    return  0;
 }
