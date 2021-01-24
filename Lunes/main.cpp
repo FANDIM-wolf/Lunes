be943879d4977663363685414ba081d175128cfe
Mikhail Shtainer <63235817+FANDIM-wolf@users.noreply.github.com>
2021-01-24 15:50:02 +0300

    Add files via upload

--- Lunes/main.cpp
+++ Lunes/main.cpp
@@ -5,23 +5,33 @@
 #include "string.h"
 using namespace  std;
 string array_text[] ={}; // array for code of coin
+long int array_text_for_id[]={};
 
 void make_coin(int amount){
     int loops = amount; // cycles of making
+    int text_id_coin;
     string text_code;
     while(loops!=0) {
         loops--;
-
         if (amount >= 100) {
             cout << "Permison is not availiable " << endl;
         }
-        for (int i = 0; i <= 23; i++) {
+        //fill array by text code
+        for (int i = 0; i <= 50; i++) {
             text_code += char(rand() % 26 + 0x61);
+
             array_text[i]=text_code;
         }
+        for (int i=24; i<=73; i++){
+            text_code += char(rand()%26+0x61);
+            array_text[i]=text_code;
+        }
+        text_id_coin += rand()%123+9999999;
+        array_text_for_id[1]=text_id_coin;
     }
 
 }
+
 int main() {
 
     cout << "Lunes\n\n\n" << std::endl;
@@ -32,6 +42,7 @@ int main() {
         cout<<"There is file "<<endl;
     }
     make_coin(1);
-    file<<""<<array_text;
+    file<<"@LUNES---"<<array_text<<array_text_for_id;
+
     return  0;
 }
--- Lunes/struct_memory.cpp
+++ Lunes/struct_memory.cpp
@@ -2,6 +2,10 @@
 // Created by mikhail-shtainer on 1/23/21.
 //
 // structs memory of coins
+#include "string.h"
 
-
+struct  Coin_Lunes {
+    long  int ID;
+    char  Text_code;
+};
 
